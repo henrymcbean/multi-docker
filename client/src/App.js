a27dca0ec1c96aa112bab4c1d2c24e060796a9d2
henrymcbean <henry_mcbean@hotmail.com>
2020-06-02 00:39:36 +0100

    Made a small change to check AWS.

--- client/src/App.js
+++ client/src/App.js
@@ -11,7 +11,7 @@ function App() {
       <div className="App">
         <header className="App-header">
           <img src={logo} height={300} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Welcome to Multi-docker</h1>
           <Link to="/">Home</Link>
           <Link to="/otherpage">Other Page</Link>
         </header>
